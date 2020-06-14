80acf838fb96018c5d190b3701cadf3ae3eb4d1b
Liza <liza8361@yandex.ru>
2020-06-14 04:09:08 +0300

    fix build and wc

--- bash/src/main/java/bash/CommandExecutor.java
+++ bash/src/main/java/bash/CommandExecutor.java
@@ -1,6 +1,8 @@
 package bash;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 // CommandExecutor исполняет команды
 public class CommandExecutor {
@@ -89,57 +91,85 @@ public class CommandExecutor {
     // Выполняет команду wc. Если команда не первая в очереди pipe и у нее нет аргументов -- то
     // в качестве входа ей подается результат предыдущей команды.
     public String executeWc(String[] args, String resultOfLastCommand, int pipePosition) {
+        if (args.length == 0 & pipePosition > 1) {
+            return computeWcFromString(resultOfLastCommand);
+        }
+        if (args.length == 0) {
+            return "error count of args";
+        } else {
+            return computeWcResultFromFile(args[0]);
+        }
+    }
+
+
+    private String computeWcResultFromFile(String fileName) {
         String result = "";
         long linesCount = 0;
         long wordsCount = 0;
         long bytesCount = 0;
-        if (args.length == 0 & pipePosition > 1) {
-            int counter = 0;
-            for (char c : resultOfLastCommand.toCharArray()) {
-                if (c == '\n') {
-                    counter += 1;
-                }
-            }
-            bytesCount += counter;
-            String[] splitArgs = resultOfLastCommand.split("\n");
-            linesCount = splitArgs.length;
-            for (String str : splitArgs) {
-                bytesCount += str.length();
-                String[] splits = parser.splitByWhitespace(str);
-                wordsCount += splits.length;
-            }
-            result = result + String.valueOf(linesCount) + " " + String.valueOf(wordsCount) + " " + String.valueOf(bytesCount);
-            return result;
-        }
-        if (args.length < 1) {
-            return "error arguments for wc";
-        }
-        File file = new File(args[0]);
-        BufferedReader reader = null;
-        try {
-            reader = new BufferedReader(new FileReader(file));
-            String text = null;
-            while ((text = reader.readLine()) != null) {
-                linesCount += 1;
-                String[] arr = text.split("\\s+");
-                if (arr.length != 0) {
-                    if (!arr[0].equals("")) {
-                        wordsCount += arr.length;
-                    }
-                }
-            }
-            bytesCount = file.length();
+        File file = new File(fileName);
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            linesCount = computeCountOfLines(reader);
         } catch (FileNotFoundException e) {
             return "cat: file does not exist";
         } catch (IOException e) {
             return "catch IO exception";
-        } finally {
-            try {
-                if (reader != null) {
-                    reader.close();
-                }
-            } catch (IOException e) {
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            wordsCount = computeCountOfWords(reader);
+        } catch (FileNotFoundException e) {
+            return "cat: file does not exist";
+        } catch (IOException e) {
+            return "catch IO exception";
+        }
+        bytesCount = file.length();
+        result = result + String.valueOf(linesCount) + " " + String.valueOf(wordsCount) + " " + String.valueOf(bytesCount);
+        return result;
+    }
+
+    private int computeCountOfLines(BufferedReader reader) throws IOException {
+        int c = 1;
+        int linesCount = 0;
+        while ((c = reader.read()) >= 0) {
+            if ((char) c == '\n') {
+                linesCount += 1;
             }
+        }
+        return linesCount;
+    }
+
+    private int computeCountOfWords(BufferedReader reader) throws IOException {
+        String text = null;
+        int wordsCount = 0;
+        while ((text = reader.readLine()) != null) {
+            String[] arr = text.trim().split("\\s+");
+            if (arr.length != 0) {
+                if (!arr[0].equals("")) {
+                    wordsCount += arr.length;
+                }
+            }
+        }
+        return wordsCount;
+    }
+
+    private String computeWcFromString(String text) {
+        long linesCount = 0;
+        long wordsCount = 0;
+        long bytesCount = 0;
+        String result = "";
+        int counter = 0;
+        for (char c : text.toCharArray()) {
+            if (c == '\n') {
+                counter += 1;
+            }
+        }
+        bytesCount += counter;
+        String[] splitArgs = text.split("\n");
+        linesCount = splitArgs.length;
+        for (String str : splitArgs) {
+            bytesCount += str.length();
+            String[] splits = parser.splitByWhitespace(str);
+            wordsCount += splits.length;
         }
         result = result + String.valueOf(linesCount) + " " + String.valueOf(wordsCount) + " " + String.valueOf(bytesCount);
         return result;
@@ -165,17 +195,17 @@ public class CommandExecutor {
     // Выполняет команду bash
     // Возвращает её output в виде строки.
     public String executeOtherCommand(String command) throws IOException {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         Process process = new ProcessBuilder("bash", "-c", command).start();
         BufferedReader errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
         String line;
         while ((line = errorReader.readLine()) != null) {
-            result += line;
+            result.append(line);
         }
         BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
         while ((line = reader.readLine()) != null) {
-            result += line;
+            result.append(line);
         }
-        return result;
+        return result.toString();
     }
 }
